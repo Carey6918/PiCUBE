a7c25e309d6a8870fe4a95029dee18d9ad3825b3
151250045 <151250045@smail.nju.edu.cn>
2017-10-31 19:17:54 +0800

    Initial commit

---
+++ routes/hello.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Dell on 2017/10/19.
+ */
+var express = require('express');
+var router = express.Router();
+
+var fs = require("fs");
+var file='./database/test.db';
+
+
+var sqlite = require('sqlite3');
+var db = new sqlite.Database(file);
+
+
+/* GET home page. */
+router.get('/', function(req, res,next) {
+    console.log(req.query)
+    var result=false;
+    db.all("SELECT password FROM user where username=?",[req.query.first_name] , function(err, rows) {
+        if(!err){
+            if(rows[0].password==req.query.last_name){
+                result=true;
+            } 
+        }
+        return res.render('login',{res:result});
+    });
+});
+
+module.exports = router;
---
+++ routes/login.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Administrator on 2017/10/31.
+ */
+var express = require('express');
+var router = express.Router();
+
+var fs = require("fs");
+var file='./database/test.db';
+
+
+var sqlite = require('sqlite3');
+var db = new sqlite.Database(file);
+
+
+/* GET home page. */
+router.get('/login', function(req, res,next) {
+    console.log(req.query);
+    console.log("调用到Login");
+    return res.render('homepage',{ title: 'Homepage' });
+});
+router.get('/toregister', function(req, res,next) {
+    res.render('register',{ title: 'Rigister' });
+});
+router.get('/register', function(req, res,next) {
+
+    res.render('homepage',{ title: 'Homepage' });
+});
+module.exports = router;
